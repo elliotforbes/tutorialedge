67ad9bbd59c0cf7a0a73b33299effd9ecdb384ed
emforce <emforce@live.com>
2015-05-25 23:43:00 +0100

    updated demo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
            <div class="col-md-3">
                <div class="course-box">
                     <img class="img-responsive image-responsive-centered" alt="test alt" title="test title" src="//lh5.ggpht.com/bkmfzv_U-lHdif9miQDh9bquTN-d35VM6c1TGIHJmmCr7YzFmgWB0EtawFh4zadk02BDo5VP2zmCdfOc9g=s355#w=1724&amp;h=1060" />
-                     <h3><a href="#">LWJGL 3 Tutorials</a></h3>
+                     <h3><a href="/single">LWJGL 3 Tutorials</a></h3>
                     <div>
                     test post excerpt
                     </div>
