114fd39cea1c01806b8b77cb9e48fe0fe1e679ce
emforce <emforce@live.com>
2015-06-03 17:11:23 +0100

    changed syntax highlighter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,9 +11,7 @@
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-   
-    <link rel="stylesheet" href="{{ asset('/highlight/styles/default.css') }}">
-    <script src="{{ asset('/highlight/highlight.pack.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/css/prism.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/front-end.css?v=1') }}">
 </head>
 
@@ -187,6 +185,7 @@
      
     <script>hljs.initHighlightingOnLoad();</script>
     <script src="{{ asset('/js/horizontalMenu.js') }}"></script>
+    <script src="{{ asset('/js/prism.js') }}"></script>
     <script>
         $(function () {
             cbpHorizontalMenu.init();
