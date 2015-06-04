415934377a80aeac41202244ee9011a38c55abf2
emforce <emforce@live.com>
2015-06-04 21:41:37 +0100

    updating responsiveness

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@ In-depth game programming courses
                    <h1>In-Depth Programming Tutorials for All Skill Levels</h1>
                </div>
            </div>
-           <div class="col-lg-6 col-md-6 col-sm-6 col-sx-6">
+           <div class="col-lg-6 col-md-6 col-sm-12 col-sx-12">
                <div class="child">
                    <div class="featured-image">
                        <img src="{{ asset('/images/featured-img.png') }}" alt="TutorialEdge.net | Programming Tutorials">
@@ -35,7 +35,7 @@ In-depth game programming courses
         <div class="row"> 
            
            @foreach ($courses as $course)
-            <div class="col-md-3 ">
+            <div class="col-md-3 col-sm-6 col-sx-12">
                <div class="course-box">
                     <img class="img-responsive image-responsive-centered" alt="{{ $course->slug }}" title="{{ $course->slug }}" src="//lh5.ggpht.com/bkmfzv_U-lHdif9miQDh9bquTN-d35VM6c1TGIHJmmCr7YzFmgWB0EtawFh4zadk02BDo5VP2zmCdfOc9g=s355#w=1724&amp;h=1060" />
                     <h3><a href="/course/{{ $course->slug }}">{{ $course->title }}</a></h3>
