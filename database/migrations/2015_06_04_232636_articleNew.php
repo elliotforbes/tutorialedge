be8be9c689feec2903d5f16e4e10b31142c2fea4
emforce <emforce@live.com>
2015-06-05 00:31:07 +0100

    new articles table

---
+++ database/migrations/2015_06_04_232636_articleNew.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ArticleNew extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+        Schema::create('articles', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('title');
+            $table->text('body');
+			$table->timestamps();
+            $table->timestamp('published_at');
+            $table->text('excerpt');
+            $table->string('slug');
+            $table->string('cat_id');
+            $table->string('image_url');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
