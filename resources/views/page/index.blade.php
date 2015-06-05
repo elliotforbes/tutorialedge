1ff1ef6f0872ff0b7dfa70ab606829efda9d5a33
emforce <emforce@live.com>
2015-06-05 01:05:09 +0100

    removed small bug

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
            @foreach ($articles as $article)
             <div class="col-md-3">
-                <img class="img-responsive image-responsive-centered" alt="Woman writing in a notebook" title="Woman writing in a notebook" src="/uploads/articles/{{ $article->image_url') }}">
+                <img class="img-responsive image-responsive-centered" alt="Woman writing in a notebook" title="Woman writing in a notebook" src="/uploads/articles/{{ $article->image_url }}">
                 <h3><a href="/{{ $article->slug }}">{{ $article->title }}</a></h3>
                 <div>
                     {{ $article->excerpt }}
