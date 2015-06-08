a12ea215f1e73c1e3d2c97ca3422589ec9825755
emforce <emforce@live.com>
2015-06-08 13:38:54 +0100

    added new pages table and changed edit page to remove risky delete button

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,8 @@
 <html lang="en">
 <head>
     <title>@yield('title') | TutorialEdge.net</title>
-    
     @yeild('meta')
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="description" content="A Tutorial site focused on bringing you high quality programming courses that will advance your knowledge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="author" href="https://plus.google.com/b/115194233692529836662">
     <meta property="og:site_name" content="Tutorial-Edge.com"/>
