12a315c14fb19232b62726706cfc6480b80aec9a
emforce <emforce@live.com>
2015-06-09 01:02:13 +0100

    fixed layouts on categories page

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -5,7 +5,7 @@
        <h2>Category Homepage</h2>
         <div class="row">
            @foreach ($articles as $article)
-            <div class="col-sm-3 col-xs-3 col-lg-3 col-md-3">
+            <div class="col-sm-6 col-xs-12 col-lg-3 col-md-4">
                 <img class="img-responsive image-responsive-centered" alt="Woman writing in a notebook" title="Woman writing in a notebook" src="/uploads/articles/{{ $article->image_url }}">
                 <h3><a href="/{{ $article->slug }}">{{ $article->title }}</a></h3>
                 <p>
