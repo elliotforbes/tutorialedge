583d48617f694faba5b3429ab5bd0e966ca9170b
emforce <emforce@live.com>
2015-06-09 01:09:23 +0100

    a few administration updates

--- resources/views/admin/articles.blade.php
+++ resources/views/admin/articles.blade.php
@@ -13,6 +13,7 @@
                      <th>excerpt</th>
                      <th>slug</th>
                      <th>cat_id</th>
+                     <th>view</th>
                      <th>edit</th>
                      <th>delete</th>
                  </tr>
@@ -26,6 +27,7 @@
                      <td>{{ $article->excerpt }}</td>
                      <td>{{ $article->slug }}</td>
                      <td>{{ $article->cat_id }}</td>
+                     <td><a href="/{{ $article->slug }"><button class="btn btn-default"></button></a></td>
                      <td><a href="/admin/edit/{{ $article->slug }}"><button class="btn btn-default">Edit</button></a></td>
                      <td>{!! Form::open(['method' => 'DELETE', 'url' => 'admin/edit/' . $article->slug]) !!}
                                 <div class="form-group">
