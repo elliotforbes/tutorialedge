07759f96169fb54099e0299ccb94fc707c6a2725
emforce <emforce@live.com>
2015-06-10 02:21:53 +0100

    misspelling

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -47,7 +47,7 @@
             </div>
             <div class="form-group">
                 {!! Form::label('Image URL') !!}
-                {!! Form::text('imagg_url', null,
+                {!! Form::text('image_url', null,
                     array('required',
                         'class'=>'form-control',
                         'placeholder'=>'Post_slug')) !!}
