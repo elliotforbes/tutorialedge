3ee322fe5d5ab659ae2711bc2cddba68d390fc32
emforce <emforce@live.com>
2015-06-10 18:45:31 +0100

    updated article font spacing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,5 +3,24 @@
 @section('content')
      <div class="latest-posts">
          <h2>Admin Panel</h2>  
+         <div class="row">
+             <div class="col-lg-6">
+                 <div class="admin-widget">
+                     <h3>Latest Comments</h3>
+                 </div>
+                 <div class="admin-widget">
+                     <h3>Quick Links</h3>
+                 </div>
+             </div>
+             <div class="col-lg-6">
+                 <div class="admin-widget">
+                     <h3>Weekly Traffic Stats</h3>
+                 </div>
+                 
+                 <div class="admin-widget">
+                     <h3>Subscribers</h3>
+                 </div>
+             </div>
+         </div>
      </div>
 @endsection
