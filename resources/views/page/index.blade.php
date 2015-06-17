d9f1b638856585982b7001a6b455e5bdb6ab56f2
emforce <emforce@live.com>
2015-06-17 12:22:30 +0100

    updates to row heights

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -9,8 +9,8 @@
 @section('content')
      <div class="latest-posts">
        <h2>{{ $page->title }}</h2>
-        <div class="row">
-           @foreach ($articles as $article)
+        @foreach ($articles as $article)
+          <div class="row">
            <div class="article-box">
                 <div class="col-sm-6 col-xs-12 col-lg-3 col-md-4">
                     <img class="img-responsive image-responsive-centered" alt="Woman writing in a notebook" title="Woman writing in a notebook" src="/uploads/articles/{{ $article->image_url }}">
@@ -24,7 +24,7 @@
     -->
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 @endsection
