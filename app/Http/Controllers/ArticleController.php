6b29e6105498479b9019ed89082eeb3594a27c01
emforce <emforce@live.com>
2015-06-19 16:50:52 +0100

    updates

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -73,7 +73,7 @@ class ArticleController extends Controller {
 	public function show($slug)
 	{
         $article = Article::whereSlug($slug)->get()->first();
-//        $articles = DB::select(DB::raw('select * from articles where cat_id = ' . $article->cat_id . ' ORDER BY title;'));
+        $articles = DB::select(DB::raw('select * from articles where cat_id = ' . $article->cat_id . ' ORDER BY title;'));
         if(is_null($article))
         {
             abort(404);   
