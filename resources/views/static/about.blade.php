9e97a8c8ababa4cc65fdf08abf06cee2e35744ea
emforce <emforce@live.com>
2015-07-05 17:39:12 +0100

    updates

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -16,6 +16,9 @@
                         Tutorial-Edge.com was originally created in January of 2015 as a hobby project of mine and has the main goal of becoming the best online resource for beginners to learn basic programming.                        
                     </p>
                     
+                    <p>
+                        <iframe src='//cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1xmH39OVgmvKycXl1I6JubYRbLXMh-dkf2AcLPjVErO0&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>
+                    </p>
                 </div>
                 
             </div>
