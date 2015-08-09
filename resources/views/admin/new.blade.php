f042b95c3d2581ee5ffe1dbfbf0d3143b4500c7e
emforce <emforce@live.com>
2015-08-09 17:07:08 +0100

    updating admin section

--- resources/views/admin/new.blade.php
+++ resources/views/admin/new.blade.php
@@ -22,7 +22,33 @@
                     </ul>
                 </div>
                 <div class="col-sm-offset-3 col-lg-9 content">
-                     @yield('content')
+                    <h2>Admin Dashboard</h2>
+                    
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="info-box">
+                                <h2>Traffic Stats</h2>
+                                <p>traffic stats go here.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="info-box">
+                                <h2>Recent Comments</h2>
+                            </div>
+                        </div>
+                        
+                        <div class="clear"></div>
+                        
+                        <div class="col-lg-4">
+                            <div class="info-box"></div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="info-box"></div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="info-box"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
