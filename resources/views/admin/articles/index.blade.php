8019f04693c94ff7984463a45e8280110e525da8
emforce <emforce@live.com>
2015-08-09 18:29:29 +0100

    never saved a file

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -39,7 +39,7 @@
              @endforeach
              </tbody>
          </table>
-         <div class="pagination"> {{ $users->links() }} </div>
+         <div class="pagination"> {{ $articles->render() }} </div>
      </div>
      
 <!--Adds confirmation popup to delete buttons so that accidental deletion doesn't take place-->
