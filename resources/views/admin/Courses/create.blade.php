c848f1e469fa5ad3003a2e9a4a6899a27fd68c4d
emforce <emforce@live.com>
2015-08-09 17:45:19 +0100

    updates

--- resources/views/admin/Courses/create.blade.php
+++ resources/views/admin/Courses/create.blade.php
@@ -5,44 +5,48 @@
 
 {!! Form::open(array('route' => 'admin.pages.store', 'class' => 'form', 'files'=>true, 'novalidate' => 'novalidate')) !!}
     <div class="row">
-        <div class="col-lg-8">
-           <h2>Create a New Course</h2>
+       <div class="info-box">
+            <div class="col-lg-8">
+               <h2>Create a New Course</h2>
+                    <div class="form-group">
+                        {!! Form::label('Course Title') !!}
+                        {!! Form::text('title', null,
+                            array('required', 
+                                'class'=>'form-control',
+                                'placeholder'=>'Post Title')) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('Course Info') !!}
+                        {!! Form::textarea('info', null,
+                            array('required',
+                                'class'=>'form-control',
+                                'placeholder'=>'Article Body')) !!}
+                    </div>
+            </div>
+        </div>
+        <div class="info-box">
+            <div class="col-lg-4">
+                <h2>Publishing Options</h2>
                 <div class="form-group">
-                    {!! Form::label('Course Title') !!}
-                    {!! Form::text('title', null,
-                        array('required', 
-                            'class'=>'form-control',
-                            'placeholder'=>'Post Title')) !!}
-                </div>
-                
-                <div class="form-group">
-                    {!! Form::label('Course Info') !!}
-                    {!! Form::textarea('info', null,
+                    {!! Form::label('Post Slug') !!}
+                    {!! Form::text('slug', null,
                         array('required',
                             'class'=>'form-control',
-                            'placeholder'=>'Article Body')) !!}
+                            'placeholder'=>'Post_slug')) !!}
                 </div>
-        </div>
-        <div class="col-lg-4">
-            <h2>Publishing Options</h2>
-            <div class="form-group">
-                {!! Form::label('Post Slug') !!}
-                {!! Form::text('slug', null,
-                    array('required',
-                        'class'=>'form-control',
-                        'placeholder'=>'Post_slug')) !!}
-            </div>
-            <div class="form-group">
-                {!! Form::label('Category ID') !!}
-                {!! Form::text('cat_id', null,
-                    array('required',
-                        'class'=>'form-control',
-                        'placeholder'=>'cat_id')) !!}
-            </div>
+                <div class="form-group">
+                    {!! Form::label('Category ID') !!}
+                    {!! Form::text('cat_id', null,
+                        array('required',
+                            'class'=>'form-control',
+                            'placeholder'=>'cat_id')) !!}
+                </div>
 
-            <div class="form-group">
-                {!! Form::submit('Submit',
-                    array('class'=>'btn btn-primary')) !!}
+                <div class="form-group">
+                    {!! Form::submit('Submit',
+                        array('class'=>'btn btn-primary')) !!}
+                </div>
             </div>
         </div>
     </div>
--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -20,7 +20,6 @@
                         <li><a href="/admin/articles">Articles</a></li>
                         <li><a href="/admin/pages">Courses</a></li>
                         <li><a href="/admin/users">Users</a></li>
-                        <li><a href="/admin/create">New Article</a></li>
                         
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Account <span class="caret"></span></a>
