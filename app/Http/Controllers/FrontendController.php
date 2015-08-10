220f8a9f6bfaa47c71863db1514f9c7c7a978dbc
emforce <emforce@live.com>
2015-08-10 20:15:51 +0100

    updates

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -19,7 +19,7 @@ class FrontendController extends Controller {
 	public function index()
 	{
 		$courses = Page::get();
-        $articles = Article::orderBy('id', 'DESC')->get();
+        $articles = Article::orderBy('id', 'DESC')->take(8)->get();
         return view('index', compact('courses', 'articles'));
 	}
     
