556f7bfcd5841fd2811e833586191eb49f9c6347
emforce <emforce@live.com>
2015-08-10 22:40:26 +0100

    updated video model

--- app/Video.php
+++ app/Video.php
@@ -4,6 +4,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model {
 
-	//
+	protected $fillable = [
+         'title',
+        'desc',
+        'published_at',
+        'course_id',
+        'slug',
+        'image_url',
+    ];
 
 }
