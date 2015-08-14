42cf7e907362dec5e7ec44b8065e14ecffe9f37d
emforce <emforce@live.com>
2015-08-14 16:03:34 +0100

    removing mass assignment error

--- app/Category.php
+++ app/Category.php
@@ -5,5 +5,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model {
 
 	//
+    protected $fillable = [
+        'title',
+        'slug',
+        'image_url',
+        'info',
+        'cat_id',
+        'created_at',
+        'updated_at'
+    ];
 
 }
