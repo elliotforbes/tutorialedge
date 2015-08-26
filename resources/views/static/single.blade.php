36b45e0dd0ed480b35945848d85e9054c7dc060f
emforce <emforce@live.com>
2015-08-26 14:10:46 +0100

    added responsive ad to sidebar

--- resources/views/static/single.blade.php
+++ resources/views/static/single.blade.php
@@ -71,6 +71,17 @@
                     </tbody>
                 </table>
                 
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Tutorial-Edge-Sidebar -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-6782067367590597"
+                     data-ad-slot="6639668084"
+                     data-ad-format="auto"></ins>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+                
                 </div>
             </div>
         </div>
