c1b519d225d20ea50cc3af947549039f660af2c4
emforce <emforce@live.com>
2015-08-28 12:08:55 +0100

    udpateds

--- resources/views/static/single.blade.php
+++ resources/views/static/single.blade.php
@@ -27,84 +27,14 @@
                </div>
                 <div class="post-content">
                     <div class="share-buttons">
-                        <!-- Social Button HTML -->
-
-                            <!-- Twitter -->
-                            <a href="http://twitter.com/share?url=<?php echo $_GET['page']; ?>&via=@tutorialedge" target="_blank" class="share-btn twitter">
-                                <i class="fa fa-twitter"></i>
-                            </a>
-
-                            <!-- Google Plus -->
-                            <a href="https://plus.google.com/share?url=<?php echo $_GET['page']; ?>" target="_blank" class="share-btn google-plus">
-                                <i class="fa fa-google-plus"></i>
-                            </a>
-
-                            <!-- Facebook -->
-                            <a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo $_GET['page']; ?>" target="_blank" class="share-btn facebook">
-                                <i class="fa fa-facebook"></i>
-                            </a>
-
-                            <!-- StumbleUpon (url, title) -->
-                            <a href="http://www.stumbleupon.com/submit?url=<?php echo $_GET['page']; ?>&title={{ $article->title }}" target="_blank" class="share-btn stumbleupon">
-                                <i class="fa fa-stumbleupon"></i>
-                            </a>
-
-                            <!-- Reddit (url, title) -->
-                            <a href="http://reddit.com/submit?url=<?php echo $_GET['page']; ?>&title={{ $article->title }}" target="_blank" class="share-btn reddit">
-                                <i class="fa fa-reddit"></i>
-                            </a>
-
-                            <!-- LinkedIn -->
-                            <a href="http://www.linkedin.com/shareArticle?url=<?php echo $_GET['page']; ?>&title={{ $article->title }}" target="_blank" class="share-btn linkedin">
-                                <i class="fa fa-linkedin"></i>
-                            </a>
-
-                            <!-- Email -->
-                            <a href="mailto:?subject=<SUBJECT&body=<BODY>" target="_blank" class="share-btn email">
-                                <i class="fa fa-envelope"></i>
-                            </a>
+                       
                     </div>
                     {!! $article->body !!}
                     <br/>
                     <h2>Share This Tutorial:</h2>
                     <br/>
                     <div class="share-buttons">
-                       <!-- Social Button HTML -->
-                            <!-- Twitter -->
-                            <a href="http://twitter.com/share?url=<?php echo $_GET['page']; ?>&via=@tutorialedge" target="_blank" class="share-btn twitter">
-                                <i class="fa fa-twitter"></i>
-                            </a>
-
-                            <!-- Google Plus -->
-                            <a href="https://plus.google.com/share?url=<?php echo $_GET['page']; ?>" target="_blank" class="share-btn google-plus">
-                                <i class="fa fa-google-plus"></i>
-                            </a>
-
-                            <!-- Facebook -->
-                            <a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo $_GET['page']; ?>" target="_blank" class="share-btn facebook">
-                                <i class="fa fa-facebook"></i>
-                            </a>
-
-                            <!-- StumbleUpon (url, title) -->
-                            <a href="http://www.stumbleupon.com/submit?url=<?php echo $_GET['page']; ?>&title={{ $article->title }}" target="_blank" class="share-btn stumbleupon">
-                                <i class="fa fa-stumbleupon"></i>
-                            </a>
-
-                            <!-- Reddit (url, title) -->
-                            <a href="http://reddit.com/submit?url=<?php echo $_GET['page']; ?>&title={{ $article->title }}" target="_blank" class="share-btn reddit">
-                                <i class="fa fa-reddit"></i>
-                            </a>
-
-                            <!-- LinkedIn -->
-                            <a href="http://www.linkedin.com/shareArticle?url=<?php echo $_GET['page']; ?>&title={{ $article->title }}" target="_blank" class="share-btn linkedin">
-                                <i class="fa fa-linkedin"></i>
-                            </a>
-
-                            <!-- Email -->
-                            <a href="mailto:?subject=<SUBJECT&body=<BODY>" target="_blank" class="share-btn email">
-                                <i class="fa fa-envelope"></i>
-                            </a>
-                    </div>
+          </div>
                 </div>
             
             </div>
