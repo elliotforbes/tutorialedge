39685f82649a03551aa7ecaa0046d4b403ac4fe0
emforce <emforce@live.com>
2015-08-30 14:16:06 +0100

    updates:

--- app/Course.php
+++ app/Course.php
@@ -6,4 +6,14 @@ class Course extends Model {
 
 	//
 
+    protected $fillable = [
+        'title',
+        'slug',
+        'image_url',
+        'info',
+        'cat_id',
+        'created_at',
+        'updated_at'
+    ];
+    
 }
