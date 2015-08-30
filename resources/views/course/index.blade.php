c8479d856a510296a3e81f78dc9792470fc5e044
emforce <emforce@live.com>
2015-08-30 16:28:55 +0100

    updates

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -11,13 +11,15 @@
             <div class="col-lg-4 course-item">
                 <div class="course-content">
                    <img src="/uploads/courses/{{ $course->image_url }}" title="{{ $course->title }}" alt="{{ $course->info }}">
-                    <h2>{{ $course->title }}</h2>
+                    <h2><a href=""></a>{{ $course->title }}</h2>
                     <p>{{ $course->info }}</p>
                 </div>
             </div>
             @endforeach
         </div>
     </div>
+    
+    <div class="white-padding"></div>
 </div>
 
 @endsection
