6362f2d30c4c86009550b05cdeef1fecc4b19389
emforce <emforce@live.com>
2015-08-31 23:05:53 +0100

    updates

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -74,7 +74,7 @@
                                            <td>Basic Plan</td>
                                        </tr>
                                         <tr>
-                                            <td>Åccess to Everything on the site.</td>
+                                            <td>Access to Everything on the site.</td>
                                             <td>No Adverts on the Site</td>
                                             <td>$4.99/month</td>
                                             <td><button class="btn btn-subscribe">Buy Now</button></td>
