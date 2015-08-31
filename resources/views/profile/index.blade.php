3c0849ec9236541179b4fd7f369a1ed4fdacf321
emforce <emforce@live.com>
2015-08-31 20:26:33 +0100

    updates

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,20 +4,22 @@
   
   <div class="gray-padding"></div>
   
-   <div class="container">
+   <div class="container ">
     <div class="post-cont">
         <div class="row">
             <div class="col-md-12">
                <div class="col-md-4 post-image">
                    <img src="{{ asset('images/profile-placeholder.gif') }}" alt="">
                </div>
-                <div class="col-md-4 post-title">
+                <div class="col-md-8 post-title">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Menu 1</a></li>
+                        <li><a href="#">Menu 2</a></li>
+                        <li><a href="#">Menu 3</a></li>
+                    </ul>
                    <h2>{{ $user->name }}</h2>
                    <p>Joined: {{ $user->created_at }}</p>
-                </div>
-                
-                <div class="col-md-4 post-content">
-                    <p>About Me:</p>
                 </div>
                 
             </div>
