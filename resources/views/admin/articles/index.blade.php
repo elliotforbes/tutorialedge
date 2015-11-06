08d90071777d1345d960314376b64c3f649dd2c8
emforce <emforce@live.com>
2015-11-06 23:32:37 +0000

    update

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -31,45 +31,47 @@
 <!-- END Stats -->
 
 
-<div class="container">
-     <div class="info-box"> 
-         <a href="/admin/articles/create"><button class="btn btn-default btn-new">New Article</button></a>
-         
-         <table class="table table-striped">
-             <thead>
-                 <tr>
-                     <th>#</th>
-                     <th>Title</th>
-                     <th>Date Published</th>
-                     <th>slug</th>
-                     <th>cat_id</th>
-                     <th>view</th>
-                     <th>edit</th>
-                     <th>delete</th>
-                 </tr>
-             </thead>
-             <tbody>
-             @foreach ($articles as $article)
-                 <tr>
-                     <td>{{ $article->id }}</td>
-                     <td>{{ $article->title }}</td>
-                     <td>{{ $article->published_at }}</td>
-                     <td>{{ $article->slug }}</td>
-                     <td>{{ $article->cat_id }}</td>
-                     <td><a href="/{{ $article->slug }}"><button class="btn btn-default">View</button></a></td>
-                     <td><a href="/admin/articles/{{ $article->slug }}/edit"><button class="btn btn-default">Edit</button></a></td>
-                     <td>{!! Form::open(['method' => 'DELETE', 'url' => 'admin/articles/' . $article->slug .'/destroy' , 'onsubmit' => 'return ConfirmDelete()']) !!}
-                                <div class="form-group">
-                                    {!! Form::submit('Delete', ['class'=>'btn btn-danger', ]) !!}
-                                </div>
-                         {!! Form::close() !!}
-                     </td>
-                 </tr>
-             @endforeach
-             </tbody>
-         </table>
-         
-         <div class="pagination"> <?php echo $articles->render(); ?> </div>
+<div class="content content-narrow">
+    <div class="block">
+        <div class="info-box"> 
+             <a href="/admin/articles/create"><button class="btn btn-default btn-new">New Article</button></a>
+
+             <table class="table table-striped">
+                 <thead>
+                     <tr>
+                         <th>#</th>
+                         <th>Title</th>
+                         <th>Date Published</th>
+                         <th>slug</th>
+                         <th>cat_id</th>
+                         <th>view</th>
+                         <th>edit</th>
+                         <th>delete</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                 @foreach ($articles as $article)
+                     <tr>
+                         <td>{{ $article->id }}</td>
+                         <td>{{ $article->title }}</td>
+                         <td>{{ $article->published_at }}</td>
+                         <td>{{ $article->slug }}</td>
+                         <td>{{ $article->cat_id }}</td>
+                         <td><a href="/{{ $article->slug }}"><button class="btn btn-default">View</button></a></td>
+                         <td><a href="/admin/articles/{{ $article->slug }}/edit"><button class="btn btn-default">Edit</button></a></td>
+                         <td>{!! Form::open(['method' => 'DELETE', 'url' => 'admin/articles/' . $article->slug .'/destroy' , 'onsubmit' => 'return ConfirmDelete()']) !!}
+                                    <div class="form-group">
+                                        {!! Form::submit('Delete', ['class'=>'btn btn-danger', ]) !!}
+                                    </div>
+                             {!! Form::close() !!}
+                         </td>
+                     </tr>
+                 @endforeach
+                 </tbody>
+             </table>
+
+             <div class="pagination"> <?php echo $articles->render(); ?> </div>
+         </div>
      </div>
 </div>
 <!--Adds confirmation popup to delete buttons so that accidental deletion doesn't take place-->
