c036d710b949a45cd1f0944631f5950028b83bfb
emforce <emforce@live.com>
2015-11-19 22:01:32 +0000

    updates

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -7,25 +7,30 @@
 @endsection
 
 @section('content')
+   <div class="gray-bg">
     <div class="container">
      <div class="latest-posts">
        <h2>{{ $category->title }}</h2>
         
           <div class="row">
             @foreach($articles as $article)
-            <div class="article-box">
-                <div class="col-sm-6 col-xs-12 col-lg-4 col-md-4">
-                    <img class="img-responsive image-responsive-centered article-image" alt="{{ $article->excerpt }}" title="{{ $article->excerpt }}" src="/uploads/articles/{{ $article->image_url }}">
-                    <div class="article-info">
-                        <h3><a href="{{ url('/') }}/{{ $article->slug }}">{{ $article->title }}</a></h3>
-<!--                        <p> </p>-->
+             <!-- Story -->
+            <div class="col-md-4">
+                <a class="block block-link-hover2" href="{{ url('/') }}/{{ $article->slug }}">
+                    <img class="img-responsive" src="/uploads/articles/{{ $article->image_url }}" alt="{{ $article->title }}">
+                    <div class="block-content">
+                        <div class="push">
+                            <span class="text-primary">{{ date("d M, Y",strtotime($article->published_at)) }}</span>
+                        </div>
+                        <h4>{{ $article->title }}</h4>
                     </div>
-                </div>
+                </a>
             </div>
             @endforeach
          </div>
          <div class="pagination">
          </div>
+    </div>
     </div>
     </div>
 @endsection
