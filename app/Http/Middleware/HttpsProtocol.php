b01cea7365ab1e637a535d39f658dc1974b97796
emforce <emforce@live.com>
2016-03-06 12:13:02 +0000

    force https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -1,15 +1,23 @@
-<?php namespace MyApp\Http\Middleware;
+<?php namespace App\Http\Middleware;
 
 use Closure;
 
 class HttpsProtocol {
 
-    public function handle($request, Closure $next)
-    {
-            if (!$request->secure() && env('APP_ENV') === 'prod') {
-                return redirect()->secure($request->getRequestUri());
-            }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
 
-            return $next($request); 
-    }
+        return $next($request); 
+	}
+
 }
