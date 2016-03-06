030d7951efd7c3f11ca21e1f97af4307b13a169c
emforce <emforce@live.com>
2016-03-06 12:10:17 +0000

    force redirect to https

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,15 @@
+<?php namespace MyApp\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol {
+
+    public function handle($request, Closure $next)
+    {
+            if (!$request->secure() && env('APP_ENV') === 'prod') {
+                return redirect()->secure($request->getRequestUri());
+            }
+
+            return $next($request); 
+    }
+}
