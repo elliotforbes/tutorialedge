c0da81bd5c0cc52b5a634baafd078714596f0fe2
emforce <emforce@live.com>
2016-03-06 20:10:06 +0000

    back to highlight

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,8 +13,8 @@
  		<link rel="stylesheet" href="{{ secure_asset('/fonts/flaticon/flaticon.css', Request::secure()) }}">
  		<link rel="stylesheet" href="{{ secure_asset('/css/material-icons.css', Request::secure()) }}">
  		<link rel="stylesheet" href="{{ secure_asset('/css/bootstrap.min.css', Request::secure()) }}">
- 		<link rel="stylesheet" href="https://tutorialedge.net/css/prism.css">
-         <link rel="stylesheet" href="{{ secure_asset('/css/site-style.css', Request::secure()) }}">
+         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css">
+         <link rel="stylesheet" href="{{ secure_asset('/css/site-style.css?v=12', Request::secure()) }}">
         <link rel="icon" type="image/png" href="{{ secure_asset('/images/favicon.ico', Request::secure()) }}">
         <script>
             (function (i, s, o, g, r, a, m) {
@@ -147,7 +147,8 @@
 		</footer>
         
         @yield('includes')
-        <script src="{{ secure_asset('/js/all.js', Request::secure()) }}"></script>
-        <script src="https://tutorialedge.net/js/prism.js"></script>
+        <script type="text/javascript" data-cfasync="false" src="{{ secure_asset('/js/all.js', Request::secure()) }}"></script>
+        <script type="text/javascript" data-cfasync="false" src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js"></script>
+        <script>hljs.initHighlightingOnLoad();</script>
 	</body>
 </html>
