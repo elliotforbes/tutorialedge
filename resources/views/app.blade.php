d160de6feb8294dc2c61fc028fa643c1cee1e8ee
emforce <emforce@live.com>
2016-03-06 17:09:45 +0000

    another shitty change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,8 +13,8 @@
  		<link rel="stylesheet" href="{{ secure_asset('/fonts/flaticon/flaticon.css', Request::secure()) }}">
  		<link rel="stylesheet" href="{{ secure_asset('/css/material-icons.css', Request::secure()) }}">
  		<link rel="stylesheet" href="{{ secure_asset('/css/bootstrap.min.css', Request::secure()) }}">
- 		<link rel="stylesheet" href="{{ secure_asset('/css/site-style.css', Request::secure()) }}">
-		<link rel="stylesheet" href="//tutorialedge.net/css/prism.css">
+ 		<link rel="stylesheet" href="//tutorialedge.net/css/prism.css">
+         <link rel="stylesheet" href="{{ secure_asset('/css/site-style.css', Request::secure()) }}">
         <link rel="icon" type="image/png" href="{{ secure_asset('/images/favicon.ico', Request::secure()) }}">
         <script>
             (function (i, s, o, g, r, a, m) {
