0a94dbfb3eebce36b344526fbee46432c9f83b4b
emforce <emforce@live.com>
2016-03-06 16:12:19 +0000

    .net

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
  		<link rel="stylesheet" href="{{ secure_asset('/fonts/flaticon/flaticon.css', Request::secure()) }}">
  		<link rel="stylesheet" href="{{ secure_asset('/css/material-icons.css', Request::secure()) }}">
  		<link rel="stylesheet" href="{{ secure_asset('/css/bootstrap.min.css', Request::secure()) }}">
-        <link rel="stylesheet" href="https://tutorialedge/net/css/prism.css">
+        <link rel="stylesheet" href="https://tutorialedge.net/css/prism.css">
  		<link rel="stylesheet" href="{{ secure_asset('/css/site-style.css', Request::secure()) }}">
 		<link rel="icon" type="image/png" href="{{ secure_asset('/images/favicon.ico', Request::secure()) }}">
         <script>
