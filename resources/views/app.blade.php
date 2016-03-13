7a418fca149122db7d25b9988bdaacfbddb52d2f
emforce <emforce@live.com>
2016-03-13 18:51:48 +0000

    updates to profile page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
  		<link rel="stylesheet" href="{{ secure_asset('/css/bootstrap.min.css', Request::secure()) }}">
 
          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css">
-         <link rel="stylesheet" href="{{ secure_asset('/css/site-style.css?v=32', Request::secure()) }}">
+         <link rel="stylesheet" href="{{ secure_asset('/css/site-style.css?v=35', Request::secure()) }}">
         <link rel="icon" type="image/png" href="{{ secure_asset('/images/favicon.ico', Request::secure()) }}">
         <script>
             (function (i, s, o, g, r, a, m) {
